efa7170c4eb717deea21ad1bfcc87b7d09edaff3
Evandro Gaspar Ribeiro <evandro@frengee.com>
2023-07-02 19:18:03 -0300

    feat: past section

--- widget-notifications/src/components/Widget.tsx
+++ widget-notifications/src/components/Widget.tsx
@@ -18,7 +18,7 @@ export function Widget() {
                     <div className="bg-zinc-900 px-8 py-4 flex items-start gap-6">
                         <Rocket className="w-6 h-6 text-violet-500 mt-3" />
                         <div className="flex-1 flex flex-col gap-2">
-                            <p className="text-sm leading-relaxed text-zinc-100 ">You received a <strong>invitation</strong> to be part of the <strong>Rocketseat</strong> company.</p>
+                            <p className="text-sm leading-relaxed text-zinc-100">You received a <strong>invitation</strong> to be part of the <strong>Rocketseat</strong> company.</p>
                             <div className="text-xxs text-zinc-400 flex items-center gap-2">
                                 <span>Invite</span>
                                 <span>3 minutes ago</span>
@@ -28,7 +28,36 @@ export function Widget() {
                     <div className="bg-zinc-900 px-8 py-4 flex items-start gap-6">
                         <Rocket className="w-6 h-6 text-violet-500 mt-3" />
                         <div className="flex-1 flex flex-col gap-2">
-                            <p className="text-sm leading-relaxed text-zinc-100 ">You received a <strong>invitation</strong> to be part of the <strong>Rocketseat</strong> company.</p>
+                            <p className="text-sm leading-relaxed text-zinc-100">You received a <strong>invitation</strong> to be part of the <strong>Rocketseat</strong> company.</p>
+                            <div className="text-xxs text-zinc-400 flex items-center gap-2">
+                                <span>Invite</span>
+                                <span>3 minutes ago</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* PAST SECTION */}
+            <div>
+                <div className="bg-zinc-950 px-5 py-2 text-sm text-zinc-400">
+                    Past
+                </div>
+
+                <div className="divide-y-2 divide-zinc-950">
+                    <div className="bg-zinc-900 px-8 py-4 flex items-start gap-6">
+                        <Rocket className="w-6 h-6 text-violet-500 mt-3" />
+                        <div className="flex-1 flex flex-col gap-2">
+                            <p className="text-sm leading-relaxed text-zinc-400">You received a <strong>invitation</strong> to be part of the <strong>Rocketseat</strong> company.</p>
+                            <div className="text-xxs text-zinc-400 flex items-center gap-2">
+                                <span>Invite</span>
+                                <span>3 minutes ago</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="bg-zinc-900 px-8 py-4 flex items-start gap-6">
+                        <Rocket className="w-6 h-6 text-violet-500 mt-3" />
+                        <div className="flex-1 flex flex-col gap-2">
+                            <p className="text-sm leading-relaxed text-zinc-400">You received a <strong>invitation</strong> to be part of the <strong>Rocketseat</strong> company.</p>
                             <div className="text-xxs text-zinc-400 flex items-center gap-2">
                                 <span>Invite</span>
                                 <span>3 minutes ago</span>
