df4fbe3bb8e33950c02c86cbad9aff6fa53a0e95
Evandro Gaspar Ribeiro <evandro@frengee.com>
2023-07-02 19:26:59 -0300

    feat: add buttons on notification

--- widget-notifications/src/components/Widget.tsx
+++ widget-notifications/src/components/Widget.tsx
@@ -1,4 +1,4 @@
-import { Rocket } from 'lucide-react'
+import { Rocket, X, Check } from 'lucide-react'
 
 export function Widget() {
     return (
@@ -34,9 +34,18 @@ export function Widget() {
                                 <span>3 minutes ago</span>
                             </div>
                         </div>
+                        <div className="flex gap-2 self-center">
+                            <button className="w-8 h-8 rounded flex items-center justify-center bg-zinc-800 hover:bg-zinc-700">
+                                <X className="w-3 h-3 text-zinc-50" />
+                            </button>
+                            <button className="w-8 h-8 rounded flex items-center justify-center bg-violet-500 hover:bg-violet-600">
+                                <Check className="w-3 h-3 text-zinc-50" />
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
+
             {/* PAST SECTION */}
             <div>
                 <div className="bg-zinc-950 px-5 py-2 text-sm text-zinc-400">
