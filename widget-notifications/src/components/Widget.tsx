656d1fa7d78b34d681a7c955bd2fbcb6fecbc30a
Evandro Gaspar Ribeiro <evandro@frengee.com>
2023-07-02 20:43:10 -0300

    feat: impl white/dark theme

--- widget-notifications/src/components/Widget.tsx
+++ widget-notifications/src/components/Widget.tsx
@@ -4,38 +4,38 @@ export function Widget() {
     return (
         <div className="w-[448px] rounded overflow-hidden">
             {/* HEADER */}
-            <div className="bg-zinc-800 py-4 px-6 flex items-center justify-between">
+            <div className="bg-zinc-200 dark:bg-zinc-800 py-4 px-6 flex items-center justify-between">
                 <span className="font-bold">Notifications</span>
                 <button className="text-xs font-bold text-violet-500 uppercase hover:text-violet-400">mark all as read</button>
             </div>
             {/* RECENT SECTION */}
             <div>
-                <div className="bg-zinc-950 px-5 py-2 text-sm text-zinc-400">
+                <div className="bg-zinc-300 font-medium dark:bg-zinc-950 px-5 py-2 text-sm text-zinc-500 dark:text-zinc-400">
                     Recent
                 </div>
 
-                <div className="divide-y-2 divide-zinc-950">
-                    <div className="bg-zinc-900 px-8 py-4 flex items-start gap-6">
+                <div className="divide-y-2 divide-zinc-300 dark:divide-zinc-950">
+                    <div className="bg-zinc-200 dark:bg-zinc-900 px-8 py-4 flex items-start gap-6">
                         <Rocket className="w-6 h-6 text-violet-500 mt-3" />
                         <div className="flex-1 flex flex-col gap-2">
-                            <p className="text-sm leading-relaxed text-zinc-100">You received a <strong>invitation</strong> to be part of the <strong>Rocketseat</strong> company.</p>
+                            <p className="text-sm leading-relaxed text-zinc-600 dark:text-zinc-100">You received a <strong>invitation</strong> to be part of the <strong>Rocketseat</strong> company.</p>
                             <div className="text-xxs text-zinc-400 flex items-center gap-2">
                                 <span>Invite</span>
                                 <span>3 minutes ago</span>
                             </div>
                         </div>
                     </div>
-                    <div className="bg-zinc-900 px-8 py-4 flex items-start gap-6">
+                    <div className="bg-zinc-200 dark:bg-zinc-900 px-8 py-4 flex items-start gap-6">
                         <Rocket className="w-6 h-6 text-violet-500 mt-3" />
                         <div className="flex-1 flex flex-col gap-2">
-                            <p className="text-sm leading-relaxed text-zinc-100">You received a <strong>invitation</strong> to be part of the <strong>Rocketseat</strong> company.</p>
+                            <p className="text-sm leading-relaxed text-zinc-600 dark:text-zinc-100">You received a <strong>invitation</strong> to be part of the <strong>Rocketseat</strong> company.</p>
                             <div className="text-xxs text-zinc-400 flex items-center gap-2">
                                 <span>Invite</span>
                                 <span>3 minutes ago</span>
                             </div>
                         </div>
                         <div className="flex gap-2 self-center">
-                            <button className="w-8 h-8 rounded flex items-center justify-center bg-zinc-800 hover:bg-zinc-700">
+                            <button className="w-8 h-8 rounded flex items-center justify-center bg-zinc-400 hover:bg-zinc-500 dark:bg-zinc-800 dark:hover:bg-zinc-700">
                                 <X className="w-3 h-3 text-zinc-50" />
                             </button>
                             <button className="w-8 h-8 rounded flex items-center justify-center bg-violet-500 hover:bg-violet-600">
@@ -48,12 +48,12 @@ export function Widget() {
 
             {/* PAST SECTION */}
             <div>
-                <div className="bg-zinc-950 px-5 py-2 text-sm text-zinc-400">
+                <div className="bg-zinc-300 font-medium dark:bg-zinc-950 px-5 py-2 text-sm text-zinc-500 dark:text-zinc-400">
                     Past
                 </div>
 
-                <div className="divide-y-2 divide-zinc-950">
-                    <div className="bg-zinc-900 px-8 py-4 flex items-start gap-6">
+                <div className="divide-y-2 divide-zinc-300 dark:divide-zinc-950">
+                    <div className="bg-zinc-200 dark:bg-zinc-900 px-8 py-4 flex items-start gap-6">
                         <Rocket className="w-6 h-6 text-violet-500 mt-3" />
                         <div className="flex-1 flex flex-col gap-2">
                             <p className="text-sm leading-relaxed text-zinc-400">You received a <strong>invitation</strong> to be part of the <strong>Rocketseat</strong> company.</p>
@@ -63,7 +63,7 @@ export function Widget() {
                             </div>
                         </div>
                     </div>
-                    <div className="bg-zinc-900 px-8 py-4 flex items-start gap-6">
+                    <div className="bg-zinc-200 dark:bg-zinc-900 px-8 py-4 flex items-start gap-6">
                         <Rocket className="w-6 h-6 text-violet-500 mt-3" />
                         <div className="flex-1 flex flex-col gap-2">
                             <p className="text-sm leading-relaxed text-zinc-400">You received a <strong>invitation</strong> to be part of the <strong>Rocketseat</strong> company.</p>
